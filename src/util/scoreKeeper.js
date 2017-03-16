f44c858cf7efb59b486ad6e53feec93de1b3b7ad
Gregory Skiano <gregoryskiano@gmail.com>
2017-03-15 23:38:08 -0700

    getScore passes all unit tests

--- src/util/scoreKeeper.js
+++ src/util/scoreKeeper.js
@@ -1,6 +1,9 @@
 export const TOTAL_POINTS = 20;
 
-
-export default function getPoints(seenCounts) {
-  return seenCounts.reduce();
-}
+export const getPoints = seenCounts => (
+  seenCounts.reduce((total, count) => {
+    if (count <= 2) return total + TOTAL_POINTS;
+    const points = TOTAL_POINTS - (2 * (count - 2));
+    return points > 0 ? points : 0;
+  }, 0)
+);
